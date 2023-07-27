284755e6aaa446b34da3cfe612aeb5478a3e820a
cybertest317 <138774114+cybertest317@users.noreply.github.com>
2023-07-27 07:14:28 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "✨",
+            react: "🎭",
             filename: __filename
         },
         async(Void, citel, text) => {
